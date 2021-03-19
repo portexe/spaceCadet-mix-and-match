360a89b2f3dbb8e299d8592f1e1d0da45813872b
susanmarie87 <susan.marie87@gmail.com>
2021-03-19 20:02:51 +0000

    Added Javascript from failed MS2

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,29 @@
-const card = document.querySelectorAll('.memory-card');
+//flip when clicked
+
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function cardAnimation(cardId) {
+    document.getElementById(cardId).style.transform = "rotateY(180deg)";
+}
 
 function flipCard() {
-    console.log('I was clicked!');
-    console.log(this);
+    this.classList.add('flip');
+    //first click
+    hasFlippedCard = true;
+    firstCard = this;
+    
+    // do cards match?
+
+  if (firstCard.dataset.framework === secondCard.dataset.framework) {
+      firstCard.removeEventListener("click",flipCard);
+      secondCard.removeEventListener("click", flipCard);
+  }
+  console.log("Function was executed!")
 }
+
+
+
 cards.forEach(card => card.addEventListener('click', flipCard));
