b3905075883cb01b80093f016e73bdded42924ef
susanmarie87 <susan.marie87@gmail.com>
2021-03-19 19:34:18 +0000

    Added constant for card storage & for loops

---
+++ assets/js/scripts.js
@@ -0,0 +1,7 @@
+const card = document.querySelectorAll('.memory-card');
+
+function flipCard() {
+    console.log('I was clicked!');
+    console.log(this);
+}
+cards.forEach(card => card.addEventListener('click', flipCard));
